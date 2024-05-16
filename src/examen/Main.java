f1ce8737e7c25283a50c782e227ab1602ce6ca06
MoliLeggo <160652112+MoliLeggo@users.noreply.github.com>
2024-05-16 17:43:01 +0200

    Examen

---
+++ src/examen/Main.java
@@ -0,0 +1,13 @@
+package examen;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String nombre = "Mariano";
+		System.out.println("Hola "+ nombre + " , perteneces a la generacion del futuro");
+		// TODO Auto-generated method stub
+
+	}
+
+}
